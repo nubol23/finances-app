148228e19769fe8b8d1df0d3c26c211dae87c96a
nubol23 <rafael.rvp98@gmail.com>
2022-05-21 15:56:15 -0400

    integrated with formik

--- src/components/auth/LoginScreen/index.tsx
+++ src/components/auth/LoginScreen/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "./login.scss";
 import { Button, Paper, TextField, Typography } from "@mui/material";
+import { FormikHelpers, useFormik } from "formik";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const LoginScreen = () => {
+  const handleLogin = (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    console.log(values);
+    setSubmitting(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: handleLogin,
+  });
+
   return (
     <div className="center-container">
       <Paper elevation={2}>
-        <form className="form-box">
+        <form className="form-box" onSubmit={formik.handleSubmit}>
           <Typography
             variant="h5"
             component="div"
@@ -18,19 +40,23 @@ const LoginScreen = () => {
 
           <TextField
             id="email"
-            label="email"
+            label="Correo"
             variant="outlined"
             size="small"
-            sx={{ marginBottom: 4 }}
+            sx={{ marginBottom: 2 }}
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
 
           <TextField
             id="password"
-            label="password"
+            label="Contraseña"
             type="password"
             variant="outlined"
             size="small"
             sx={{ marginBottom: 4 }}
+            value={formik.values.password}
+            onChange={formik.handleChange}
           />
 
           <div className="row">
