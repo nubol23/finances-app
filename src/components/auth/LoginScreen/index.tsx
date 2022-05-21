cc6dbde30952d6a8cd7ec09e4a0079198c08e7fb
nubol23 <rafael.rvp98@gmail.com>
2022-05-21 14:51:47 -0400

    created login form skeleton

--- src/components/auth/LoginScreen/index.tsx
+++ src/components/auth/LoginScreen/index.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import "./login.scss";
+import { Button, Paper, TextField, Typography } from "@mui/material";
 
 const LoginScreen = () => {
-  return <div>:D</div>;
+  return (
+    <div className="center-container">
+      <Paper elevation={2}>
+        <form className="form-box">
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            sx={{ marginBottom: 3 }}
+          >
+            Econ App 💵
+          </Typography>
+
+          <TextField
+            id="email"
+            label="email"
+            variant="outlined"
+            size="small"
+            sx={{ marginBottom: 4 }}
+          />
+
+          <TextField
+            id="password"
+            label="password"
+            type="password"
+            variant="outlined"
+            size="small"
+            sx={{ marginBottom: 4 }}
+          />
+
+          <div className="row">
+            <Button fullWidth variant="outlined" type="submit">
+              Login
+            </Button>
+
+            <Button fullWidth variant="outlined">
+              Registrar
+            </Button>
+          </div>
+        </form>
+      </Paper>
+    </div>
+  );
 };
 
 export default LoginScreen;
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginScreen from "../components/auth/LoginScreen";
+import "./app_router.scss";
 
 const AppRouter = () => {
   return (
-    <>
+    <div className="site-container">
       <Routes>
         <Route path="login" element={<LoginScreen />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
